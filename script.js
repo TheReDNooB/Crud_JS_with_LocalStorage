8bb1912bc256c83b9945dfcd7b664b7ef5b93f88
TheReDNooB <xmiguelx209@gmail.com>
2023-09-05 08:42:01 -0500

    ajuste css

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function DeleteData(index) {
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
-        icon: 'warning',
+        icon: 'info',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
